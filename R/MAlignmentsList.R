d583cfea8d7f73d59fc9e93d980fdac47a3f1c8e
ttriche <tim.triche@gmail.com>
2018-06-20 19:31:26 -0400

    better formatting for show(MAlignmentsList)

--- R/MAlignmentsList.R
+++ R/MAlignmentsList.R
@@ -123,7 +123,7 @@ setMethod("show", signature(object="MAlignmentsList"),
           function(object) {
             cat("MAlignmentsList object of length", length(object), "\n")
             cat("-------\n", sep = "")
-            cat("Summary(object) returns a ")
+            cat("Summary(object) returns\n")
             show(Summary(object))
             cat("-------\n", sep = "")
             cat("seqinfo: ", summary(seqinfo(object)), "\n", sep = "")
