9089a221c997ba34f3e25162910e3769e95a529f
ttriche <tim.triche@gmail.com>
2018-06-20 16:38:37 -0400

    fix what seems to have been a bug in callMT on lists

--- R/callMT.R
+++ R/callMT.R
@@ -20,61 +20,65 @@ callMT <- function(mal, ..., parallel=FALSE, verbose=FALSE){
   } else if (is(mal, "MAlignmentsList")) { 
     message("Variant-calling an MAlignmentsList (this may melt your machine).")
     if (parallel == TRUE) { 
-      message("Trying to call variants in parallel from R is doubleplus ungood")
+      stop("Parallel mtDNA variant calling is not working at the moment.") 
       return(MVRangesList(mclapply(mal, callMT)))
     } else { 
       return(MVRangesList(lapply(mal, callMT))) 
     }
-  }
+  } else { 
 
-  mtChr <- seqlevelsInUse(mal)
-  mtGenome <- unique(genome(mal))
-  gmapGenome <- paste("GmapGenome", "Hsapiens", mtGenome, sep=".")
-  if (verbose) {
-    message("Attempting to load ", gmapGenome, " as reference genome index...") 
-  }
-
-  if (!requireNamespace(gmapGenome)) {
-    if (mtGenome == "rCRS") {
-      stop("You can use indexMtGenome() to build a suitable ", gmapGenome, ".")
+    mtChr <- seqlevelsInUse(mal)
+    mtGenome <- unique(genome(mal))
+    gmapGenome <- paste("GmapGenome", "Hsapiens", mtGenome, sep=".")
+    if (verbose) {
+      message("Attempting to load ",gmapGenome," as reference genome index...") 
     }
+
+    if (!requireNamespace(gmapGenome)) {
+      if (mtGenome == "rCRS") {
+        stop("MTseeker::indexMtGenome() will build a suitable ",gmapGenome,".")
+      }
+    }
+
+    try(attachNamespace(gmapGenome), silent=TRUE)
+    supportedSeqLevels <- seqlevels(get(gmapGenome)) 
+    if (!mtChr %in% supportedSeqLevels) { 
+      stop(mtChr, " is not among the supported seqlevels in ", gmapGenome, ".")
+    }
+
+    # for accounting purposes: 
+    bamFile <- fileName(mal) 
+    isCircular(mal) <- FALSE # for variant calling
+    pars <- TallyVariantsParam(get(gmapGenome), 
+                               minimum_mapq=20L,
+                               high_base_quality=20L,
+                               ignore_duplicates=TRUE, 
+                               read_length=as(runLength(mal), "integer"),
+                               which=as(seqinfo(mal)[mtChr], "GRanges"),
+                               indels=TRUE)
+
+
+    # needed for feedback during sometimes-slow bulk variant calling 
+    message("Tallying variants for ", bamFile, "...") 
+    tallied <- tallyVariants(bamFile, pars)
+
+    if (verbose) message("QA'ing variants...") 
+    QAed <- qaVariants(tallied)
+
+    if (verbose) message("Calling ", bamFile, " vars against ", mtGenome, "...")
+    filters <- VariantCallingFilters(...)
+    res <- callVariants(QAed, calling.filters=filters)
+
+    if (verbose) message("Filtering variants...")
+    sampleNames(res) <- gsub(paste0(".", mtGenome), "", 
+                             gsub("\\.bam", "", basename(bamFile)))
+    res$PASS <- apply(softFilterMatrix(res), 1, all) == 1
+    res$VAF <- altDepth(res) / totalDepth(res)
+    genome(res) <- mtGenome
+
+    if (verbose) message("Formatting variants...") 
+    mvr <- MVRanges(res, coverage(mal))
+    names(mvr) <- mtHGVS(mvr)
+    return(mvr)
   }
-
-  try(attachNamespace(gmapGenome), silent=TRUE)
-  supportedSeqLevels <- seqlevels(get(gmapGenome)) 
-  if (!mtChr %in% supportedSeqLevels) { 
-    stop(mtChr, " is not among the supported seqlevels in ", gmapGenome, ".")
-  }
-
-  isCircular(mal) <- FALSE # for variant calling
-  pars <- TallyVariantsParam(get(gmapGenome), 
-                             minimum_mapq=20L,
-                             high_base_quality=20L,
-                             ignore_duplicates=TRUE, 
-                             read_length=as(runLength(mal), "integer"),
-                             which=as(seqinfo(mal)[mtChr], "GRanges"),
-                             indels=TRUE)
-  
-  if (verbose) message("Tallying variants...") 
-  tallied <- tallyVariants(fileName(mal), pars)
-
-  if (verbose) message("QA'ing variants...") 
-  QAed <- qaVariants(tallied)
-
-  # needed for feedback during sometimes-slow bulk variant calling 
-  message("Calling variants for ", fileName(mal), " against ", mtGenome, "...") 
-  filters <- VariantCallingFilters(...)
-  res <- callVariants(QAed, calling.filters=filters)
-
-  if (verbose) message("Filtering variants...")
-  sampleNames(res) <- gsub(paste0(".", mtGenome), "", 
-                           gsub("\\.bam", "", basename(fileName(mal))))
-  res$PASS <- apply(softFilterMatrix(res), 1, all) == 1
-  res$VAF <- altDepth(res) / totalDepth(res)
-  genome(res) <- mtGenome
-
-  if (verbose) message("Formatting variants...") 
-  mvr <- MVRanges(res, coverage(mal))
-  names(mvr) <- mtHGVS(mvr)
-  return(mvr)
 }
