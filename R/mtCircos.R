4a284de722c344e08718869226d136bd76eecddf
ttriche <tim.triche@gmail.com>
2018-07-06 00:24:22 -0400

    yet more fussing with mtCircos()

--- R/mtCircos.R
+++ R/mtCircos.R
@@ -39,7 +39,7 @@ mtCircos <- function(anno=NULL, variants=NULL, matrices=NULL) {
     gr2 <- granges(variants)
     # coding <- predictCoding(variants)
     browser()
-    circos.genomicRainfall(gr2, pch=16, cex=0.4, track.height=0.1,
+    circos.genomicRainfall(as.data.frame(gr2), pch=16, cex=.4, track.height=.1,
                            col=c("#FF000080", "#0000FF80"), bg.border=NA)
   } else { 
     circos.track(track.height=0.1, ylim=c(0,1), bg.border=NA)
