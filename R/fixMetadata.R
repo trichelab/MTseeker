3fba080fbfefa9d849215e3e701f98e9fd46fad5
ttriche <tim.triche@gmail.com>
2018-10-05 21:44:13 -0400

    more examples

--- R/fixMetadata.R
+++ R/fixMetadata.R
@@ -9,6 +9,11 @@
 #'
 #' @return    the object, with fixed metadata (if needed and possible) 
 #' 
+#' @examples
+#' library(MTseekerData)
+#' data(RONKSreads)
+#' fixed <- fixMetadata(RONKSreads)
+#' 
 #' @export
 fixMetadata <- function(x) {
   metadata(x) <- attr(validMetadata(x), "fixedMeta")
