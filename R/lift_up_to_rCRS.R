43d7ae1a8a89d7682f0b605025e3c2d6bf429d8a
Ben Johnson <ben.johnson4343@gmail.com>
2019-04-05 15:05:08 -0400

    update docs and add biscuit filter for importVCF

--- R/lift_up_to_rCRS.R
+++ R/lift_up_to_rCRS.R
@@ -1,13 +1,20 @@
-#' @import GenomicRanges
+#' Lift hg19 chrM to rCRS
+#' 
+#' @name lift_up_to_rCRS
 #' 
 #' @param ranges_obj Either GRanges or MVRanges
 #' @param verbose    logical, if set to TRUE, print all messages during progressing.
 #' @return           Either Granges or MVRanges object
 #'
+#' @import GenomicRanges
 #'
 #' referred to http://haplogrep.uibk.ac.at/blog/rcrs-vs-rsrs-vs-hg19/
 #' 
 #' @export
+#' 
+#' @examples 
+#' 
+
 lift_up_to_rCRS <- function(ranges_obj, verbose = T) {
   if(!(class(ranges_obj) == "GRanges" || class(ranges_obj) == "MVRanges")) {
     stop("Either GRanges or MVRanges must be provided as input")
