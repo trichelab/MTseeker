d07a853025c748757e426bf8bb00897f83a3fa5f
ttriche <tim.triche@gmail.com>
2019-05-23 11:42:30 -0400

    add a VCF export method for convenience

---
+++ R/exportFilteredAsVcf.R
@@ -0,0 +1,24 @@
+#' convenience wrapper for dumping filtered variants directly to an indexed VCF
+#'
+#' @param vars 	an MVRanges or MVRangesList to filter, HGVS-annotate, and dump
+#' @param file  the name of the resulting VCF file to produce (default is NULL)
+#' @param ...   other arguments, passed to filterMTvars()
+#'
+#' @return	the filtered set of variants (as an MVRanges) that was exported 
+#' 
+#' @import 	VariantAnnotation
+#'
+#' @examples
+#' library(MTseekerData) 
+#' filterMTvars(RONKSvariants$RO_1)
+#' exportFilteredAsVcf(RONKSvariants$RO_1) # not written to a file 
+#' 
+#' @export 
+exportFilteredAsVcf <- function(vars, file=NULL, ...) { 
+  
+  if (is(vars, "MVRangesList")) vars <- unlist(vars)
+  res <- filterMTvars(vars, ...)
+  if (!is.null(file)) writeVcf(res, file, index=TRUE)
+  return(res)
+
+}
